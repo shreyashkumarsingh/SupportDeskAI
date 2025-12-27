1fc4dbcb022d074b88f67cae6342a04178fca311
shreyashkumarsingh <shreyashkrsingh@gmail.com>
2025-12-27 11:08:53 +0530

    Fix path aliases for Vercel build

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tsconfigPaths from "vite-tsconfig-paths";
+import path from "path";
 
 export default defineConfig({
   server: {
     host: "::",
     port: 8080,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
   },
   plugins: [
     react(),
